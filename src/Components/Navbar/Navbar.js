e41af4b0b834dc8fa461da16e6feff2410578918
Lesther687 <jordi.lf.cb@gmail.com>
2020-09-10 23:56:58 -0300

    Development
    - Funcionalidad footer

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
-import Button from "@material-ui/core/Button";
-import Toolbar from "@material-ui/core/Toolbar";
+import {
+    Button,
+    Toolbar
+} from "@material-ui/core"
 
 function Navbar() {
     return (
